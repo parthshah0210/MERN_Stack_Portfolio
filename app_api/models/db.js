5abb6256ea52f587a17fa2075b6b7fc77112fe08
parthshah0210 <113199803+parthshah0210@users.noreply.github.com>
2024-08-04 15:21:53 -0400

    v1

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb+srv://admin:Parthshah@cluster0.9udsr.mongodb.net/myportfolio?retryWrites=true&w=majority'; mongoose.connect(dbURI);
+const dbURI = 'mongodb+srv://parth:Parthshah%402016@cluster0.0s9xob4.mongodb.net/aws?retryWrites=true&w=majority&appName=Cluster0+srv://admin:Parthshah@cluster0.9udsr.mongodb.net/myportfolio?retryWrites=true&w=majority'; mongoose.connect(dbURI);
 
 mongoose.connect(dbURI);
 
